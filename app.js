67537efdd4faa931a31dbbc418e1364d2aaf7a2f
CJ Hanson <CJ.Hanson@rhythmagency.com>
2014-07-30 09:15:32 -0700

    Scaffolded express app

--- app.js
+++ app.js
@@ -1,56 +1,59 @@
-/******************************************************************
- ***                                                          ***
- ***    Written by CJ.Hanson @ RhythmAgency.com               ***
- ***    Copyright 2014 (c) Rhythm Interactive                 ***
- ***                                                          ***
- *****************************************************************/
+var express = require('express');
+var path = require('path');
+var favicon = require('static-favicon');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
 
-var conf = require('nconf');
-var _ = require('underscore')._;
+var routes = require('./routes/index');
+var users = require('./routes/users');
 
-conf.argv().env().file({file: __dirname + '/config/config.json'});
+var app = express();
 
-process.env.PORT = conf.get("PORT");
-process.env.NODE_ENV = conf.get("NODE_ENV");
-process.env.DEBUG = conf.get("DEBUG");
-process.env.HTTP_AUTH_ENABLED = conf.get("HTTP_AUTH_ENABLED");
-process.env.HTTP_AUTH_USERNAME = conf.get("HTTP_AUTH_USERNAME");
-process.env.HTTP_AUTH_PASSWORD = conf.get("HTTP_AUTH_PASSWORD");
-process.env.NODE_DB_HOST = conf.get("NODE_DB_HOST");
-process.env.NODE_DB_PORT = conf.get("NODE_DB_PORT");
-process.env.NODE_DB_USERNAME = conf.get("NODE_DB_USERNAME");
-process.env.NODE_DB_PASSWORD = conf.get("NODE_DB_PASSWORD");
-process.env.NODE_DB_DATABASE = conf.get("NODE_DB_DATABASE");
-process.env.NODE_DB_DEBUG = conf.get("NODE_DB_DEBUG");
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
 
-var app = require('express')();
-//var db = require('./config/database')(app);
-//
-//db.connect(function() {
-//    require('./config/environment')(app);
-//    require('./config/startup')(app);
-//    require('./config/routes')(app);
-//
-//    /**
-//     * Prepare Exit Callbacks
-//     */
-//
-//        // gracefully close database on exit
-//    process.stdin.resume();
-//    process.on('SIGINT', function() {
-//        app.get('db').close();
-//        process.exit();
-//    });
+app.use(favicon());
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded());
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
 
-    /**
-     * Ready, Set, Go!
-     */
+app.use('/', routes);
+app.use('/users', users);
 
-    app.get('/', function(req, res) {
-      res.send('Hello World!');
+/// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+    var err = new Error('Not Found');
+    err.status = 404;
+    next(err);
+});
+
+/// error handlers
+
+// development error handler
+// will print stacktrace
+if (app.get('env') === 'development') {
+    app.use(function(err, req, res, next) {
+        res.status(err.status || 500);
+        res.render('error', {
+            message: err.message,
+            error: err
+        });
     });
+}
 
-    app.listen(process.env.PORT, function() {
-        console.log('Express server listening on port ' + process.env.PORT + ' in ' + process.env.NODE_ENV + ' mode');
+// production error handler
+// no stacktraces leaked to user
+app.use(function(err, req, res, next) {
+    res.status(err.status || 500);
+    res.render('error', {
+        message: err.message,
+        error: {}
     });
-//});
+});
+
+
+module.exports = app;
