35a5e78bb961fe6915b120b7789e9bce1a06efc1
CJ Hanson <CJ.Hanson@rhythmagency.com>
2014-08-01 15:13:32 -0700

    Homepage lists skills alphabetically

--- routes/views/index.js
+++ routes/views/index.js
@@ -8,6 +8,23 @@ exports = module.exports = function(req, res) {
 	// locals.section is used to set the currently selected
 	// item in the header navigation.
 	locals.section = 'home';
+
+    locals.skillBaseNames = [];
+
+    view.on('init', function(next) {
+        keystone.list('Skill').model.find().distinct('baseName').exec(function(err, results){
+            if (err || !results.length) {
+                return next(err);
+            }
+
+            results.sort(function (a, b) {
+                return a.toLowerCase().localeCompare(b.toLowerCase());
+            });
+            locals.skillBaseNames = results;
+
+            next();
+        });
+    });
 	
 	// Render the view
 	view.render('index');
