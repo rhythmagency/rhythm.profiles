9d70de131d8bfad31602ed3e14d96c7b17f211a8
CJ Hanson <CJ.Hanson@rhythmagency.com>
2014-08-11 15:53:57 -0700

    Users listed on homepage by skill ordered by level in that skill

--- routes/views/index.js
+++ routes/views/index.js
@@ -9,22 +9,67 @@ exports = module.exports = function(req, res) {
 	// item in the header navigation.
 	locals.section = 'home';
 
-    view.query('users', keystone.list('User').model.find().sort('name.last').populate({path: 'skills', options: { sort: {'level':'desc'} }}));
-
+    locals.users = [];
     locals.skillBaseNames = [];
 
     view.on('init', function(next) {
-        keystone.list('Skill').model.find().distinct('baseName').exec(function(err, results){
-            if (err || !results.length) {
+        keystone.list('User').model.find().sort('name.last').populate({path: 'skills', options: { sort: '-level' }}).exec(function(err, users){
+            if(err || !users.length){
                 return next(err);
             }
 
-            results.sort(function (a, b) {
-                return a.toLowerCase().localeCompare(b.toLowerCase());
-            });
-            locals.skillBaseNames = results;
+            locals.users = users;
+        }).then(function(){
+            keystone.list('Skill').model.find().distinct('baseName').exec(function(err, results){
+                if (err || !results.length) {
+                    return next(err);
+                }
 
-            next();
+                results.sort(function (a, b) {
+                    return a.toLowerCase().localeCompare(b.toLowerCase());
+                });
+
+                locals.skillsWithUsers = {};
+
+                results.forEach(function(skillBaseName, idx, arr){
+                    locals.skillsWithUsers[skillBaseName] = [];
+
+                    locals.users.forEach(function(user, idx2, arr2){
+                        user.skills.forEach(function(skill, idx3, arr3){
+                            if(skill.baseName == skillBaseName){
+                                locals.skillsWithUsers[skillBaseName].push(user);
+                            }
+                        });
+                    });
+
+                    locals.skillsWithUsers[skillBaseName].sort(function (a, b) {
+                        var comp = 1;
+
+                        var matched = false;
+                        a.skills.forEach(function(skillA, idxA, arrA){
+                            if(matched)
+                                return;
+                            b.skills.forEach(function(skillB, idxB, arrB){
+                                if(skillBaseName == skillA.baseName && skillBaseName == skillB.baseName){
+                                    if(skillA.level == skillB.level){
+                                        comp = 0;
+                                    }else if(skillA.level > skillB.level){
+                                        comp = -1;
+                                    }
+                                    matched = true;
+                                    return;
+                                }
+                            });
+                        });
+
+                        return comp;
+                    });
+                });
+
+                locals.skillBaseNames = results;
+
+                next();
+            });
         });
     });
 	
